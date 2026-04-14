2ecd1dd24a44f6ce628a0649dcf6ef4575125d49
Aniiita <anita_nen4eva@abv.bg>
2026-04-14 18:53:42 +0300

    Modified app.html and app.ts

--- matchly-app-frontend/src/app/app.ts
+++ matchly-app-frontend/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ToastComponent } from './core/toast/toast/toast.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ToastComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
