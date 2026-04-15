ef3acc66c756fdefc00046249d0aaf7c396b6d44
Aniiita <anita_nen4eva@abv.bg>
2026-04-15 11:15:30 +0300

    Add landing route

--- matchly-app-frontend/src/app/app.routes.ts
+++ matchly-app-frontend/src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './features/home/pages/landing-page/landing-page.component';
+import { authGuard } from './core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingPageComponent, canActivate: [authGuard] },
+];
 
