989cb8ae52c753f3da64a78b5820b25f01af029d
Aniiita <anita_nen4eva@abv.bg>
2026-04-15 11:56:59 +0300

    Add login route

--- matchly-app-frontend/src/app/app.routes.ts
+++ matchly-app-frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './features/home/pages/landing-page/landing-page.component';
+import { LoginComponent } from './features/auth/login/login.component';
 import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent, canActivate: [authGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [authGuard] },
 ];
 
