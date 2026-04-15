19aee3fdb26b18fde2071b81a96605f2e01d318c
Aniiita <anita_nen4eva@abv.bg>
2026-04-15 13:39:59 +0300

    Add register route

--- matchly-app-frontend/src/app/app.routes.ts
+++ matchly-app-frontend/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './features/home/pages/landing-page/landing-page.component';
 import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
 import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent, canActivate: [authGuard] },
   { path: 'login', component: LoginComponent, canActivate: [authGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [authGuard] },
+
 ];
 
