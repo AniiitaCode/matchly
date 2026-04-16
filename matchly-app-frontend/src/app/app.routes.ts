0c266544928f0a5799e172041ff00947b4336415
Aniiita <anita_nen4eva@abv.bg>
2026-04-16 11:36:49 +0300

    Add dating profile route

--- matchly-app-frontend/src/app/app.routes.ts
+++ matchly-app-frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './features/auth/login/login.component';
 import { RegisterComponent } from './features/auth/register/register.component';
 import { HomeComponent } from './features/home/pages/home/home.component';
 import { EditProfileComponent } from './features/profile/pages/edit-profile/edit-profile.component';
+import { EditDatingProfileComponent } from './features/profile/pages/edit-dating-profile/edit-dating-profile.component';
 import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
@@ -13,5 +14,6 @@ export const routes: Routes = [
 
   { path: 'home', component: HomeComponent, canActivate: [authGuard] },
   { path: 'profile', component: EditProfileComponent, canActivate: [authGuard] },
+  { path: 'profile-dating', component: EditDatingProfileComponent, canActivate: [authGuard] },
 ];
 
